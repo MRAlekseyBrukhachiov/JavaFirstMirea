67b5a5f98b13ad7e0d9b0fce98270887d1a41fe4
MRAlekseyBrukhachiov <aleksey.bryukhachiov@yandex.ru>
2021-09-16 00:09:02 +0300

    добавил task5, task2, task6 изменил task1

--- src/ru/mirea/task2/Main.java
+++ src/ru/mirea/task2/Main.java
@@ -2,10 +2,18 @@ package ru.mirea.task2;
 
 public class Main {
     public static void main(String[] args) {
+        //TestShape
+        Shape sh = new Shape("Blue");
+        System.out.println(sh);
+        //TestDog
         TestDog t1 = new TestDog();
         Dog d1 = new Dog();
+        Dog d2 = new Dog("Bob");
+        Dog d3 = new Dog("Rex", 5);
         Dog [] array = new Dog[0];
         array = t1.addDog(array, d1);
+        array = t1.addDog(array, d2);
+        array = t1.addDog(array, d3);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
