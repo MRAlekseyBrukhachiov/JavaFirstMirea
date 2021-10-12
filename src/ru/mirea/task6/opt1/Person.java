9108b1fce87388a2657f76240e02f9c49d40ca92
MRAlekseyBrukhachiov <aleksey.bryukhachiov@yandex.ru>
2021-10-12 11:28:44 +0300

    changed task6(1)

--- src/ru/mirea/task6/opt1/Person.java
+++ src/ru/mirea/task6/opt1/Person.java
@@ -2,8 +2,12 @@ package ru.mirea.task6.opt1;
 
 public class Person implements Nameable {
     private String name;
+
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
     public Person(String name) {
         this.name = name;
