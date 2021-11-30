731880364b82c2a2477b01d0d47fda8f562dd265
MRAlekseyBrukhachiov <aleksey.bryukhachiov@yandex.ru>
2021-12-01 02:32:56 +0300

    add task24

---
+++ src/ru/mirea/task24/Task24.java
@@ -0,0 +1,36 @@
+package ru.mirea.task24;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+
+public class Task24 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<User> users = new ArrayList<User>();
+        String login, password;
+        Pattern loginPattern = Pattern.compile("\\w*");
+        Pattern passwordPattern = Pattern.compile("^[A-Za-z0-9]{6,}$");
+
+        System.out.println("|*****РЕГИСТРАЦИЯ*****|");
+
+        while (true) {
+            System.out.print("Логин: ");
+            login = in.next();
+            System.out.print("Пароль: ");
+            password = in.next();
+            if (!loginPattern.matcher(login).matches()) {
+                System.out.println("Логин может содержать только символы латинского алфавита");
+            } else if (!passwordPattern.matcher(password).matches()) {
+                System.out.println("Длина пароль должна быть не менее 6 символов. Символами могут быть только буквы латинского алфавита и цифры");
+            } else {
+                User user = new User (login, password);
+                users.add(user);
+                break;
+            }
+        }
+
+        System.out.println("Вы успешно зарегистрировались");
+    }
+}
---
+++ src/ru/mirea/task24/User.java
@@ -0,0 +1,11 @@
+package ru.mirea.task24;
+
+public class User {
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
